fed9370f1b7f071d3b5af8e401e5496adcc67ac9
DESKTOP-4J1SBNE\alexa <korol.yula@gmail.com>
2021-08-18 21:00:02 +0700

    Задача Vector(все нестатические методы, Остальось: toString()-переделать, статические методы  и в  Main код

--- Vector/src/AcademITSchool/Vector/vector/Vector.java
+++ Vector/src/AcademITSchool/Vector/vector/Vector.java
@@ -9,11 +9,21 @@ public class Vector {
         return this.components.length;
     }
 
+    public double getComponent(int i) {
+        return this.components[i];
+    }
+
+    public void setComponent(int i,double number) {
+        this.components[i]=number;
+    }
+
     public Vector(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("Размерность вектора должна быть > 0");
         }
+
         this.components = new double[n];
+
         for (int i = 0; i < n; i++) {
             this.components[i] = 0;
         }
@@ -21,34 +31,136 @@ public class Vector {
 
     public Vector(Vector vector) {
         double[] components = new double[vector.getSize()];
+
         for (int i = 0; i < vector.getSize(); i++) {
             this.components[i] = vector.components[i];
         }
     }
 
-    public Vector(double[] vector) {
-        this.components = new double[vector.length];
-        for (int i = 0; i < vector.length; i++) {
-            this.components[i] = vector[i];
+    public Vector(double[] array) {
+        this.components = new double[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            this.components[i] = array[i];
         }
     }
 
-    public Vector(int n, double[] vector) {
+    public Vector(int n, double[] array) {
         if (n <= 0) {
             throw new IllegalArgumentException("Размерность вектора должна быть  > 0");
         }
+
         this.components = new double[n];
-        for (int i = 0; i < vector.length; i++) {
-            this.components[i] = vector[i];
+
+        for (int i = 0; i < array.length; i++) {
+            this.components[i] = array[i];
         }
-        for (int i = vector.length; i < n; i++) {
+
+        for (int i = array.length; i < n; i++) {
             this.components[i] = 0;
         }
     }
+
+    public void addAnotherVector(Vector vector) {
+        if (vector.getSize() < this.getSize()) {
+            for (int i = vector.getSize(); i < this.getSize(); i++) {
+                vector.components[i] = 0;
+            }
+        }
+
+        if (vector.getSize() > this.getSize()) {
+            for (int i = this.getSize(); i < vector.getSize(); i++) {
+                this.components[i] = 0;
+            }
+        }
+
+        for (int i = 0; i < vector.getSize(); i++) {
+            this.components[i] += vector.components[i];
+        }
+    }
+
+
+    public void subtractAnotherVector(Vector vector) {
+        if (vector.getSize() < this.getSize()) {
+            for (int i = vector.getSize(); i < this.getSize(); i++) {
+                vector.components[i] = 0;
+            }
+        }
+
+        if (vector.getSize() > this.getSize()) {
+            for (int i = this.getSize(); i < vector.getSize(); i++) {
+                this.components[i] = 0;
+            }
+        }
+
+        for (int i = 0; i < vector.getSize(); i++) {
+            this.components[i] -= vector.components[i];
+        }
+    }
+
+    public void multiplyByScalar(int scalar) {
+        for (int i = 0; i < this.getSize(); i++) {
+            this.components[i] *= scalar;
+        }
+    }
+
+    public void unfoldVector() {
+        this.multiplyByScalar(-1);
+    }
+
+    public double getVectorLength() {
+        int squaresCoordinatesSum = 0;
+
+        for (int i = 0; i < this.getSize(); i++) {
+            squaresCoordinatesSum += this.components[i] * this.components[i];
+        }
+
+        return Math.sqrt(squaresCoordinatesSum);
+    }
+
 
     @Override
     public String toString() {
         return Arrays.toString(this.components).replace("[", "{").replace("]", "}");
 
+    }
+
+    @Override
+    public boolean equals(Object vector) {
+        if (vector == this) {
+            return true;
+        }
+
+        if (vector == null || vector.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Vector v = (Vector) vector;
+
+        if (this.getSize() != v.getSize()) {
+            return false;
+        }
+
+        boolean isEqequals = true;
+
+        for (int i = 0; i < this.getSize(); i++) {
+
+            if (this.components[i] != v.components[i]) {
+                isEqequals = false;
+            }
+        }
+
+        return isEqequals;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 15;
+        int hash = 1;
+
+        for (int i = 0; i < this.getSize(); i++) {
+            hash = prime * hash + Double.hashCode(this.components[i]);
+        }
+        return hash;
     }
 }
--- Vector/src/AcademITSchool/Vector/vector_main/Main.java
+++ Vector/src/AcademITSchool/Vector/vector_main/Main.java
@@ -4,7 +4,7 @@ import AcademITSchool.Vector.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Vector vector1 = new Vector(0);
+        Vector vector1 = new Vector(1);
         double[] vv = {1, 3, 4, 10};
         Vector vector2 = new Vector(vv);
         Vector vector4 = new Vector(6, vv);
@@ -12,7 +12,10 @@ public class Main {
         System.out.println(vector1);
         System.out.println(vector2);
         System.out.println(vector3);
-        System.out.println(vector4);
+
+        System.out.println(vector2.getSize());
+        System.out.println(vector2.getComponent(1));
+
 
     }
 }

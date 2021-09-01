c033a5da8bed6598b1fb592119abe658036326c5
ula <korol.yula@gmail.com>
2021-09-01 16:26:58 +0700

    Задача  ArrayListHome  исправлена

--- ArrayListHome/src/academic/korol/Main.java
+++ ArrayListHome/src/academic/korol/Main.java
@@ -45,6 +45,7 @@ public class Main {
                     uniqueNumbersArray.add(e);
                 }
             }
+
             System.out.println(numbersArray);
             System.out.println(uniqueNumbersArray);
         }
